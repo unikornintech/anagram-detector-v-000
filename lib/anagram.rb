ccbccdfca9e086e3a5890196bc83f4f47f8aa420
Lauren Days <lmchambers0420@gmail.com>
2019-02-10 02:25:38 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+ class Anagram 
+   attr_accessor :detector 
+   
+   def initialize(word)
+     @word = word
+   end 
+   
+   def match(input)
+     input.select do |w|
+       w.split("").sort == @word.split("").sort
+     end 
+     
+   end 
+ end 
